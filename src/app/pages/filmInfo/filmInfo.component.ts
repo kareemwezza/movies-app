4d2378199880c6a63c84a0167453e6de1b68c471
amrelskhawy <amrelskhawy@gmail.com>
2022-09-14 01:33:02 +0200

    Movie Page Details undercreating

--- src/app/pages/filmInfo/filmInfo.component.ts
+++ src/app/pages/filmInfo/filmInfo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import Movie from '../../types/movie'
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-filmInfo',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmInfoComponent implements OnInit {
 
-  constructor() { }
+  movieId;
+  filmData: Movie;
+
+  items: MenuItem[] = [];
+
+  constructor(private route: ActivatedRoute) {
+    this.movieId = this.route.snapshot.paramMap.get('id');
+
+
+    this.filmData = {
+      movieId: 5,
+      movieName: "Spider man No Way Home",
+      category: "Action, Adventure",
+      image: "/../../../assets/images/spiderman.jpg",
+      year: '2022',
+      rating: '4.5'
+
+    }
+  }
+
+
 
   ngOnInit() {
+    this.items = [
+      { label: 'Categories' },
+      { label: 'Sports' },
+      { label: 'Football' },
+      { label: 'Countries' },
+      { label: 'Spain' },
+      { label: 'F.C. Barcelona' },
+      { label: 'Squad' },
+      {
+        label: 'Lionel Messi',
+        url: 'https://en.wikipedia.org/wiki/Lionel_Messi'
+      }
+    ];
   }
 
 }
