d504df796fd22206a6994fac6e0a484710c05caa
kareemwezza <kareemfouad27@gmail.com>
2022-09-13 12:58:36 +0200

    fix container on all screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { ScrollTopModule } from 'primeng/scrolltop';
 import { HttpClientModule } from '@angular/common/http';
 import { ButtonModule } from 'primeng/button';
 import { OffersModule } from './components/offers/offers.module';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,8 +32,9 @@ import { LoginRegisterComponent } from './components/login-register/login-regist
     ButtonModule,
     FormsModule,
     OffersModule,
-    ScrollTopModule,
     HttpClientModule,
+    ScrollTopModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
