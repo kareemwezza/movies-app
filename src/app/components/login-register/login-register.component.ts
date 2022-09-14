c01c2cc60f440b7be7dfe9512cba687a494c5659
amrelskhawy <amrelskhawy@gmail.com>
2022-09-14 23:38:07 +0200

    Validate

--- src/app/components/login-register/login-register.component.ts
+++ src/app/components/login-register/login-register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../../shared/services/auth.service';
 import { Observable } from 'rxjs';
 import { Message, MessageService } from 'primeng/api';
 import { Router } from '@angular/router';
+import { validateEmail } from 'src/app/utils/validate';
+import { confirmPassword } from 'src/app/utils/validate';
 
 @Component({
   selector: 'LoginRegister',
@@ -14,30 +16,63 @@ export class LoginRegisterComponent implements OnInit {
     private _authService: AuthService,
     private _messageService: MessageService,
     private _router: Router
-  ) {}
+  ) { }
 
-  @Input() type: String = 'login';
-  username: string = 'wezza';
-  email: string = 'wezzza@gmail.com';
-  mobile: string = '12345678900';
-  password: string = '123456';
-  confirmPassword: string = '123456';
+  @Input() type: String = '';
 
-  ngOnInit(): void {}
+  username: string = '';
+  email: string = '';
+  mobile: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  ngOnInit(): void { }
 
   formObservable!: Observable<any>;
 
   handleFormSubmit() {
+
     if (this.type === 'login') {
-      this.formObservable = this._authService.login(this.email, this.password);
-    } else {
-      this.formObservable = this._authService.register(
-        this.email,
-        this.username,
-        this.mobile,
-        this.password
-      );
+
+      if (!validateEmail(this.email)) {
+        this.showMessage("Email is not Valid")
+
+
+
+      } else if (this.password.length < 6) {
+        this.showMessage("Password Lenght is short")
+      } else {
+        this.formObservable = this._authService.login(this.email, this.password);
+      }
+
+
+    } else if (this.type === 'register') {
+
+
+      if (!validateEmail(this.email)) {
+        this.showMessage("Email is not Valid")
+
+
+
+      } else if (this.password.length < 6) {
+        this.showMessage("Password Lenght is short")
+
+      } else if (!confirmPassword(this.password, this.confirmPassword)) {
+          this.showMessage("Passwords Not Matched")
+      }
+      else {
+        this.formObservable = this._authService.register(
+          this.email,
+          this.username,
+          this.mobile,
+          this.password
+        );
+      }
+
     }
+
+
+
     this.formObservable.subscribe(
       resData => {
         if (this.type === 'login') {
@@ -54,6 +89,15 @@ export class LoginRegisterComponent implements OnInit {
       }
     );
   }
+
+
+
+
+
+
+
+
+
 
   showMessage(message: string, severity: string = 'error') {
     this._messageService.add({
