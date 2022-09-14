736c384bcd8c0b99adc0ac803d84f85c9d2e69c6
kareemwezza <kareemfouad27@gmail.com>
2022-09-14 13:53:10 +0200

    adding navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  showMobileMenu: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
   }
-
 }
